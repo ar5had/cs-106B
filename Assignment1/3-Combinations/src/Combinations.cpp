59ab8d1d3925924def49ea228b05c1bf64dd502e
Arshad Khan <arshdkhn1@gmail.com>
2016-08-17 07:38:21 +0530

    adding combination program

--- Assignment1/3-Combinations/src/Combinations.cpp
+++ Assignment1/3-Combinations/src/Combinations.cpp
@@ -1,10 +1,9 @@
 /*
  * File: Combinations.cpp
  * ----------------------
- * Name: [TODO: enter name here]
- * Section: [TODO: enter section leader here]
- * This file is the starter project for the recursive combinations problem.
- * [TODO: rewrite the documentation]
+ * Name: Arshad Khan
+ *
+ * Recursive implementation of C(n,k)
  */
 
 #include <iostream>
@@ -12,7 +11,34 @@
 #include "simpio.h"
 using namespace std;
 
+int combination(int n, int k);
+void printPascalTriangle(int rows);
+
 int main() {
-   // [TODO: fill in the code]
-   return 0;
+    while(true){
+        int n = getInteger("Enter 'n': ");
+        int k = getInteger("Enter 'k': ");
+        if (n < k) {
+            cout << "Enter 'k' value less than n ..." << endl;
+            continue;
+        }
+        cout << "C(n, k) => " << combination(n, k) << endl;
+        if( !getYesOrNo("Do you want to continue - 'yes' or 'no' ?") )
+            break;
+    }
+    printPascalTriangle(getInteger("How many rows of pascal triangle do you want to get printed ?"));
+    return 0;
+}
+
+int combination(int n, int k) {
+    cout << n << " " << k << endl;
+    if(n == k || k == 0)
+        return 1;
+    else
+        return combination(n - 1, k - 1) + combination(n - 1, k);
+}
+
+void printPascalTriangle(int rows) {
+    cout << "Here " << (rows == 1 ? "is the " : "are the ") << rows << " of Pascal triangle." << endl;
+
 }
