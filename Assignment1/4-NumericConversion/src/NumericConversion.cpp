8fbe461f7649c2cb2c818a92186b91ae7afcc8f2
Arshad Khan <arshdkhn1@gmail.com>
2016-08-19 07:30:01 +0530

    completed assignment 11

--- Assignment1/4-NumericConversion/src/NumericConversion.cpp
+++ Assignment1/4-NumericConversion/src/NumericConversion.cpp
@@ -9,29 +9,58 @@
 #include <iostream>
 #include <string>
 #include "console.h"
+#include "simpio.h"
+#include <cmath>
 using namespace std;
 
 /* Function prototypes */
 
 string intToString(int n);
-int stringToInt(string str);
+int stringToInt(string str, string sign="not-set");
 
 /* Main program */
 
 int main() {
-    string numStr = getLine("String: Enter no...");
-    int numInt = getInteger("Integer: Enter no...")
-    string numIntStr = intToString(numInt);
-    int numStrInt = stringToInt(numStr);
+    while(true) {
+        cout << "Enter an integer and get result in string format" << endl;
+        string numStr = intToString(getInteger("Integer: Enter no..."));
+        cout << "String form of the no is " << numStr << endl;
+        cout << "\n\n";
+        cout << "Enter a string no and get result in integer format" << endl;
+        int numInt = stringToInt(getLine("String: Enter no..."));
+        cout << "Int form of the no is " << numInt << endl;
+        cout << "\n\n";
+    }
     return 0;
 }
 
 string intToString(int n) {
-//    var digit = n % 10;
-//    n = n / 10;
-//    return string(digit)
+    if(n < 0) {
+        return "-" + intToString(-n);
+    }
+    else if(n / 10 == 0) {
+        char letter = '0' + n;
+        return letter + string();
+    } else {
+        int digit = n % 10;
+        char letter = '0' + digit;
+        return intToString(n / 10) + letter + string();
+    }
 }
 
-int stringToInt(string str) {
-
+int stringToInt(string str, string sign) {
+    if(sign == "not-set"){
+        if(str[0] == '-')
+            return -1 * stringToInt(str.substr(1, str.length()), "set");
+    }
+    if(str.length() > 0) {
+        int firstDigit = str[0] - '0';
+        if(firstDigit > 9 || firstDigit < 0)
+            throw "BAD NUMBER!";
+        return firstDigit * pow(10, str.length() - 1) + stringToInt(str.substr(1, str.length()), "set");
+    }
+    else
+        return 0;
 }
+
+
